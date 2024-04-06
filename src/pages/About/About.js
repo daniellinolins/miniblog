676eb699ce2ba83a7ffaf33ebcfbb489c14deb43
daniellinolins <daniel.lins@vohu.com.br>
2024-04-06 19:27:41 +0100

    create style and content of about page.

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,11 +1,18 @@
-import style from "./About.module.css";
+import styles from "./About.module.css";
+
+import { Link } from "react-router-dom"
 
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <div className={styles.about}>
+      <h2>
+        About Mini <span>Blog</span>
+      </h2>
+      <p>
+        This project is a Mini Blog created with React as front-end and Firebase as back-end.
+      </p>
+      <Link to="/posts/create" className="btn">New Post</Link>
     </div>
   )
 }
-
 export default About
